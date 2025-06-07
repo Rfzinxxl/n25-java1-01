ea61ad59407c79ec9e7ce3d844ad8a05141bfcdc
Raphael Barbosa Filho <epura.z@gmail.com>
2025-06-06 21:47:06 -0300

    aula12

--- Greenfoot/Aula01/MundoAbelha/Abelha.java
+++ Greenfoot/Aula01/MundoAbelha/Abelha.java
@@ -8,6 +8,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Abelha extends Actor
 {
+    int vidas;
+    int score;
+    int PONTOS=100;
+/**
+ * Constructor da Classe Abelha
+ */
+    public Abelha(){
+        vidas=3;
+        score=0;
+        
+    }
+    
     /**
      * Act - do whatever the Abelha wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -23,6 +35,9 @@ public class Abelha extends Actor
         turn(5);
     }
     verificarPosicao();
+    capturarMosca();
+  serCapturadopelaAranha();
+  
     }
     /**
      * Método que verifica posição da Abelha.
@@ -42,6 +57,7 @@ public class Abelha extends Actor
         if(estaNaEsquerda()){
             setLocation(getWorld().getWidth()-10,getY());
         }
+        
     }
 
     /**
@@ -72,4 +88,44 @@ public class Abelha extends Actor
     public boolean estaNaDireita(){
         return getX()>getWorld().getWidth()-10;
     }
+    
+    /**
+     * Metodo de Captura
+     */
+    public void capturarMosca(){
+     if(isTouching(Mosca.class)){
+         removeTouching(Mosca.class);
+         Greenfoot.playSound("slurp.wav");
+         atualizarScore();
+         int posX=Greenfoot.getRandomNumber(getWorld().getWidth())+1;
+         int posY=Greenfoot.getRandomNumber(getWorld().getHeight())+1;
+         Mosca mosca = new Mosca();
+         getWorld().addObject(mosca,posX,posY);     
+         
+     }
+    }
+    
+    /**
+     * Ser capturado pela Aranha
+     */
+    public void serCapturadopelaAranha(){
+        if(isTouching(Aranha.class)){
+          int posX=Greenfoot.getRandomNumber(getWorld().getWidth())+1;
+         int posY=Greenfoot.getRandomNumber(getWorld().getHeight())+1;  
+         setLocation(posX,posY);
+         Greenfoot.playSound("ouch.wav");
+         vidas--;
+         if(vidas<=0){
+             getWorld().showText("GAME OVER",400,300);
+             Greenfoot.stop();
+         }
+        }
+    }
+    
+    public void atualizarScore(){
+        score+=PONTOS;
+        getWorld().showText("Score:"+score,100,10);
+    }
+    
+ 
 }
--- Greenfoot/Aula01/MundoAbelha/Duke.java
+++ Greenfoot/Aula01/MundoAbelha/Duke.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Duke here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Dec0Dex
+ * 2025-06-06
  */
 public class Duke extends Actor
 {
@@ -15,6 +15,13 @@ public class Duke extends Actor
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+            Greenfoot.playSound("ouch.wav");
+        }
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if(mouse!=null && mouse.getButton()==3){
+            setLocation(mouse.getX(),mouse.getY());
     
     }
+}
 }
